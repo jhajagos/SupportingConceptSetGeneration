99a3d1dbd6934446d735a922c516c4cfb0e084c8
jhajagos <janos.hajagos@stonybrook.edu>
2025-07-23 16:04:52 -0400

    Removed IDs

--- comorbidities/generateComorbidities.R
+++ comorbidities/generateComorbidities.R
@@ -2,3 +2,13 @@
 install.packages("comorbidity")
 
 library(comorbidity)
+
+df <- comorbidity::icd10cm_2022
+df["id"] <- 1:nrow(df)
+
+icd10_cm_map_charl <- comorbidity::comorbidity(x=df,code="Code", id="id", map="charlson_icd10_quan", assign0=FALSE)
+icd10_cm_map_elix <- comorbidity::comorbidity(x=df,code="Code", id="id", map="elixhauser_icd10_quan", assign0=FALSE)
+
+write.csv(df,"./icd10_cm_map_codes.csv", row.names=FALSE)
+write.csv(icd10_cm_map_elix,"./icd10_cm_map_elix.csv")
+write.csv(icd10_cm_map_charl, "./icd10_cm_map_charl.csv", row.names = FALSE)
