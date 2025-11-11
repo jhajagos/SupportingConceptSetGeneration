5e8d634f083ed8f68527388403733300d6cdca9a
jhajagos <janos.hajagos@stonybrook.edu>
2025-11-10 20:34:33 -0500

    Added simple comparison class

--- src/concept_set.py
+++ src/concept_set.py
@@ -1,4 +1,6 @@
-class Concept(object):
+import datetime
+
+class CodedConcept(object):
     def __init__(self, code, vocabulary, description):
         self.code = code
         self.description = description
@@ -7,8 +9,54 @@ class Concept(object):
     def key(self):
         return self.code + "|" + self.vocabulary
 
+    def __repr__(self):
+        return str((self.code, self.vocabulary, self.description))
 
-class ConceptSet(object):
+
+class CodedConceptSetChange(object):
+
+    def __init__(self, added_codes_list, removed_codes_list, change_type, changed_by=None, change_type_dict=None, derived_from=None, derived_from_version=None):
+        self.added_codes = added_codes_list
+        self.removed_codes = removed_codes_list
+
+        self.change_type = change_type
+        self.derived_from = derived_from
+        self.derived_from_version = derived_from_version
+
+        self.registration_utc_datetime = None
+        self.changed_by = changed_by
+
+        if change_type_dict is None: # Holds human or machine created labels why a specific change was made
+            change_type_dict = {}
+        self.change_type_dict = change_type_dict
+
+    def __repr__(self):
+        return str((self.added_codes, self.removed_codes, self.change_type, self.registration_utc_datetime))
+
+
+class CodedConceptSetHistory(object):
+    """Concept set history contains links to all Concept Objects"""
+
+    def __init__(self, concept_set):
+        self.concept_set = concept_set
+        self.concept_dict = {} # Need to store all concept objects that are in the history
+
+        for concept in concept_set.concept_dict.values():
+            self.concept_dict[concept.key()] = concept
+
+        self.changes = []
+
+    def register_change(self, change):
+        change.registration_utc_datetime = datetime.datetime.utcnow()
+        self.changes += [change]
+        if change.added_codes is not None and len(change.added_codes) > 0: # Insure we have a Code Object for every change
+            for code in change.added_codes:
+                self.concept_dict[code.key()] = code
+
+
+
+class CodedConceptSet(object):
+    """A container for concepts"""
 
     def __init__(self, name, concepts=None, version=0):
         self.name = name
@@ -20,13 +68,15 @@ class ConceptSet(object):
                 concept_key = concept.key()
                 self.concept_dict[concept_key] = concept
 
+        self.history = CodedConceptSetHistory(self)
+
     def __and__(self, other):
         """Intersection of two concept sets"""
         set_intersection = self._keys() & other._keys()
         new_concept_list = []
         for key in set_intersection:
             new_concept_list += [self.concept_dict[key]]
-        return ConceptSet(name=self.name + " & " + other.name, concepts=new_concept_list)
+        return CodedConceptSet(name= "(" + self.name + " & " + other.name + ")", concepts=new_concept_list)
 
     def __or__(self, other):
         """Union of two concept sets"""
@@ -39,7 +89,7 @@ class ConceptSet(object):
             else:
                 new_concept_list += [other.concept_dict[key]]
 
-        return ConceptSet(name=self.name + " | " + other.name, concepts=new_concept_list)
+        return CodedConceptSet(name=self.name + " | " + other.name, concepts=new_concept_list)
 
     def __len__(self):
         return len(self.concept_dict)
@@ -50,32 +100,72 @@ class ConceptSet(object):
         new_concept_list = []
         for key in set_difference:
             new_concept_list += [self.concept_dict[key]]
-        return ConceptSet(name=self.name + " - " + other.name, concepts=new_concept_list)
+        return CodedConceptSet(name=self.name + " - " + other.name, concepts=new_concept_list)
+
+    def __iter__(self):
+        return iter(self.concept_dict.values())
+
+    def __eq__(self, other):
+        return set(self.concept_dict.keys()) == set(other.concept_dict.keys())
 
     def _keys(self):
         return set(self.concept_dict.keys())
 
-
-class ConceptSetChange(object):
-
-    def __init__(self, added_codes, removed_codes, change_type):
-        self.added_codes = added_codes
-        self.removed_codes = removed_codes
-        self.change_type = change_type
-
-class ConceptSetHistory(object):
-
-    def __init__(self, concept_set):
-        self.version = 0
-        self.concept_set = concept_set
-
     def register_change(self, change):
-        pass
+        self.history.register_change(change)
+        self.version += 1
+
+        if change.added_codes is not None and len(change.added_codes) > 0:
+            for code in change.added_codes:
+                self.concept_dict[code.key()] = code
+
+        if change.removed_codes is not None and len(change.removed_codes) > 0:
+            for code in change.removed_codes:
+                self.concept_dict.pop(code.key())
 
     def get_version(self, version):
-        pass
+        """Takes the current version and back steps to older versions. New version loses past history"""
 
-    def get_latest_version(self):
-        pass
+        local_concept_dict = dict.copy(self.concept_dict)
+        reversed_history = list(reversed(self.history.changes))
 
+        for i in range(self.version - version):
+            change_to_reverse = reversed_history[i]
+            for code in change_to_reverse.added_codes:
+                local_concept_dict.pop(code.key())
+
+            for code in change_to_reverse.removed_codes:
+                local_concept_dict[code.key()] = code
+
+        version_concept_set_name = self.name + f"(Copy: v{version})"
+
+        local_concepts = [local_concept_dict[c] for c in local_concept_dict]
+        return CodedConceptSet(name=version_concept_set_name, concepts=local_concepts)
+
+
+class CompareCodedConceptSets(object):
+    """Compares two concept sets and returns a list of differences"""
+
+    def __init__(self, concept_set_1, concept_set_2):
+        self.concept_set_1 = concept_set_1
+        self.concept_set_2 = concept_set_2
+
+        self.intersection = self.concept_set_1 & self.concept_set_2
+        self.union = self.concept_set_1 | self.concept_set_2
+
+        self.left_difference = self.concept_set_1 - self.concept_set_2
+        self.right_difference = self.concept_set_2 - self.concept_set_1
+
+    def summary(self):
+
+        print(f"{self.concept_set_1.name}: {len(self.concept_set_1)} concepts")
+        print(f"{self.concept_set_2.name}: {len(self.concept_set_2)} concepts")
+
+        print(f"Intersection: # {len(self.intersection)}")
+        print(f"Union: # {len(self.union)}")
+
+        print(f"Overlap {len(self.intersection)/len(self.union)}")
+
+        print(f"Left Difference: {len(self.left_difference)} concepts")
+        print(f"Right Difference: {len(self.right_difference)} concepts")
 
--- test/test_concept_set.py
+++ test/test_concept_set.py
@@ -1,11 +1,19 @@
 import unittest
-from concept_set import Concept, ConceptSet
+from concept_set import CodedConcept, CodedConceptSet, CodedConceptSetChange, CompareCodedConceptSets
+
+def helper_concept_list(concept_set_string):
+    concept_list = []
+    for line in concept_set_string.split("\n"):
+        code, description, vocabulary = line.split("\t")
+        concept_list += [CodedConcept(code, vocabulary, description)]
+
+    return concept_list
 
 
 class TestConcept(unittest.TestCase):
     def test_creation(self):
 
-        c1 = Concept("E11.22", "ICD10CM", "Type 2 diabetes mellitus with diabetic chronic kidney disease	")
+        c1 = CodedConcept("E11.22", "ICD10CM", "Type 2 diabetes mellitus with diabetic chronic kidney disease	")
 
         self.assertEqual(c1.key(), "E11.22|ICD10CM")  # add assertion here
 
@@ -19,10 +27,7 @@ G89.28	Other chronic postprocedural pain	ICD10CM
 G44.321	Chronic post-traumatic headache, intractable	ICD10CM
 G44.201	Tension-type headache, unspecified, intractable	ICD10CM"""
 
-       self.concept_list_1 = []
-       for line in cst_1.split("\n"):
-            code, description, vocabulary = line.split("\t")
-            self.concept_list_1 += [Concept(code, vocabulary, description)]
+       self.concept_list_1 =  helper_concept_list(cst_1)
 
        cst_2 = """G89.4	Chronic pain syndrome	ICD10CM
 G89.29	Other chronic pain	ICD10CM
@@ -30,26 +35,102 @@ G89.21	Chronic pain due to trauma	ICD10CM
 M10.059	Idiopathic gout, unspecified hip	ICD10CM
 M25.562	Pain in left knee	ICD10CM
 M25.532	Pain in left wrist	ICD10CM"""
+       self.concept_list_2 = helper_concept_list(cst_2)
 
-       self.concept_list_2 = []
-       for line in cst_2.split("\n"):
-           code, description, vocabulary = line.split("\t")
-           self.concept_list_2 += [Concept(code, vocabulary, description)]
+       cst3 = """G44.209	Tension-type headache, unspecified, not intractable	ICD10CM
+G44.029	Chronic cluster headache, not intractable	ICD10CM"""
+
+       self.concept_list_3 = helper_concept_list(cst3)
+
+       cst4 = """G44.321	Chronic post-traumatic headache, intractable	ICD10CM
+G44.201	Tension-type headache, unspecified, intractable	ICD10CM"""
+
+       self.concept_list_4 = helper_concept_list(cst4)
 
     def test_creation(self):
-        p1 = ConceptSet("Pain", self.concept_list_1)
+        p1 = CodedConceptSet("Pain", self.concept_list_1)
         self.assertTrue(len(p1) == 6)
 
     def test_union(self):
 
-        p1 = ConceptSet("Pain 1", self.concept_list_1)
-        p2 = ConceptSet("Pain 2", self.concept_list_2)
+        p1 = CodedConceptSet("Pain 1", self.concept_list_1)
+        p2 = CodedConceptSet("Pain 2", self.concept_list_2)
 
         p3 = p1 | p2
-
-        print(len(p3))
-
         self.assertTrue(len(p3) == 9)
+
+    def test_intersection(self):
+        p1 = CodedConceptSet("Pain 1", self.concept_list_1)
+        p2 = CodedConceptSet("Pain 2", self.concept_list_2)
+        p3 = p1 & p2
+
+        self.assertTrue(len(p3) == 3)
+
+    def test_difference(self):
+        p1 = CodedConceptSet("Pain 1", self.concept_list_1)
+        p2 = CodedConceptSet("Pain 2", self.concept_list_2)
+        p3 = p1 - p2
+
+        self.assertTrue(len(p3) == 3)
+
+    def test_concept_set_change(self):
+
+        p1 = CodedConceptSet("Pain 1", self.concept_list_1)
+
+        pl3 = self.concept_list_3
+
+        dx1 = pl3[0].key()
+        dx2 = pl3[1].key()
+
+        change_description_1 = {dx1: "Missed codes", dx2: "Missed codes"}
+        change_obj_1 = CodedConceptSetChange(added_codes_list=pl3, removed_codes_list=[], change_type="addition", change_type_dict=change_description_1)
+
+        self.assertIsNotNone(change_obj_1)
+
+        p1.register_change(change_obj_1)
+
+        self.assertEqual(8, len(p1))
+        self.assertEqual(1, p1.version)
+
+        pl4 = self.concept_list_4
+        dx4 = pl4[0].key()
+        dx5 = pl4[1].key()
+
+        change_description_2 = {dx4: "Codes are incorrect", dx5: "Codes are incorrect"}
+        change_obj_2 = CodedConceptSetChange(added_codes_list=[], removed_codes_list=pl4, change_type="removal",
+                                             change_type_dict=change_description_2)
+
+        p1.register_change(change_obj_2)
+
+        self.assertEqual(6, len(p1))
+        self.assertEqual(2, p1.version)
+
+        p1_v1 = p1.get_version(1)
+        self.assertEqual(8, len(p1_v1))
+
+        p1_v0 = p1.get_version(0)
+        self.assertEqual(6, len(p1_v0))
+
+        p1_recreated = CodedConceptSet("Pain 1 Recreated", self.concept_list_1)
+
+        self.assertEqual(p1_v0, p1_recreated)
+
+    def test_iteration(self):
+        p1 = CodedConceptSet("Pain 1", self.concept_list_1)
+        i = 0
+        for concept in p1:
+            self.assertIsNotNone(concept)
+            i += 1
+
+        self.assertEqual(6, i)
+
+    def test_summary(self):
+        concept_set_1 = CodedConceptSet("Pain 1", self.concept_list_1)
+        concept_set_2 = CodedConceptSet("Pain 2", self.concept_list_2)
+
+        CompareCodedConceptSets(concept_set_1, concept_set_2).summary()
+
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
